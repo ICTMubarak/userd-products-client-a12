e532634f774163786b662ba8070187eed63ce37b
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-11-23 22:15:57 -0800

    Added basic structure of navbar outlet and footer

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Footer/Footer';
+import Navbar from '../Pages/Navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
-            <h1>THa is main</h1>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
