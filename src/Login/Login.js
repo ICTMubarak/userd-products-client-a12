f7a5c73ca3a527048735803cb5a2a0c23a02ec46
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-11-24 03:43:46 -0800

    Update login page

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../contexts/UserContext';
 
 const Login = () => {
-    return (
-        <div>
-            <h2>This is login page</h2>
+
+const {signIn} = useContext(AuthContext);
+
+const handleSubmit = event => {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.email.value;
+  const password = form.password.value;
+  
+  console.log(email, password);
+
+  signIn(email, password)
+  .then(result => {
+    const user = result.user;
+    console.log(user);
+    form.reset();
+  })
+  .catch(error => console.error(error))
+
+}
+
+return (
+  <div className="hero min-h-screen bg-base-200" class="col-span-5 border-2">
+  <div className="hero-content flex-col">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Please Login now!</h1>
+    </div>
+    <form onSubmit={handleSubmit} className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" name="email" placeholder="email" className="input input-bordered" required />
         </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn">Login</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
     );
 };
 
---
+++ src/firebase/Firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVNCdGtt3YG7EE7W3sTYJNxZbdMZ5DaUk",
+  authDomain: "used-laptop-53be9.firebaseapp.com",
+  projectId: "used-laptop-53be9",
+  storageBucket: "used-laptop-53be9.appspot.com",
+  messagingSenderId: "759483178495",
+  appId: "1:759483178495:web:dec7700677518e616c5bca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app; 
